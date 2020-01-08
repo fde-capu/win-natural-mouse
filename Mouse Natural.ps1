4a608c6f3cfbb872ca4e8c4ab0a56869c28e964c
flacarrara <flacarrara@gmail.com>
2020-01-08 17:59:18 -0300

    First Commit

---
+++ Mouse Natural.ps1
@@ -0,0 +1,10 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Enum\HID\*\*\Device` Parameters FlipFlopWheel -EA 0 | ForEach-Object { Set-ItemProperty $_.PSPath FlipFlopWheel 1 }
+
+Get-PnpDevice -Class 'Mouse' |
+    Where-Object { $_.Status -eq 'OK' } |
+    ForEach-Object { $_ | Disable-PnpDevice -Confirm:$false
+                     $_ | Enable-PnpDevice -Confirm:$false }
